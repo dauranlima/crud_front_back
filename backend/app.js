683928f8f59b86dcb91e6ce5ced2354b22c735f1
dauranlima <39663453+dauranlima@users.noreply.github.com>
2024-09-04 18:02:41 -0300

    configurando routes no backend

--- backend/app.js
+++ backend/app.js
@@ -7,5 +7,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const routes = require("./routes/router")
+
+app.use("/api", routes)
 
 app.listen(3000, ()=> console.log('Server is ONLINE running at backdoor 3000'))
