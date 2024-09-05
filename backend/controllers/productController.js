40a41fac5c099102b1a1a0a8d7275faa7742e299
dauranlima <39663453+dauranlima@users.noreply.github.com>
2024-09-05 16:37:28 -0300

    configurando controlller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+const {Product: ProductModel} = require("../models/product")
+
+const productController = () => {
+
+create: async (req, res) => {
+  try {
+    
+    const product = {
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+
+
+
+
+}
