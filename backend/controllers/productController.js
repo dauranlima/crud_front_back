007a2de5584c54c0f8258be990f1ff2eed5d418f
dauranlima <39663453+dauranlima@users.noreply.github.com>
2024-09-05 17:50:34 -0300

    configurando o create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,14 @@ create: async (req, res) => {
   try {
     
     const product = {
+      title: req.body.title,
+      code: req.body.code,
+      size: req.body.size,
+      price: req.body.price
     }
+
+    const res = await ProductModel.create(product)
+    res.status(201).json({res, msg:"Product added successfully"})
   } catch (err) {
     console.log(err)
   }
---
+++ backend/routes/product.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+const productController = require('../controllers/productController')
+
+router.route('/product').post(()=> productController.create(req,res))
+
+router.route('/product').get(()=> productController.getAll(req,res))
+
+router.route('/product').get(()=> productController.get(req,res))
+
+router.route('/product').delete(()=> productController.delete(req,res))
+
+router.route('/product').put(()=> productController.update(req,res))
+
+
+module.exports = router;
