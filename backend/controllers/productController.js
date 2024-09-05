b632afcb2942ab0e49a2ec1b556ee9750b6205aa
dauranlima <39663453+dauranlima@users.noreply.github.com>
2024-09-05 18:45:47 -0300

    configurando os controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const {Product: ProductModel} = require("../models/product")
 
-const productController = () => {
+const productController =  {
 
 create: async (req, res) => {
   try {
@@ -17,11 +17,75 @@ create: async (req, res) => {
   } catch (err) {
     console.log(err)
   }
+},
+
+getAll: async(req,res) => {
+  try {
+    
+    const product = await ProductModel.find();
+    res.json(product)
+
+  } catch (error) {
+    console.log(error)
+  }
+},
+
+get: async (req,res) => {
+  try {
+    const id = req.params.id
+    const product = await ProductModel.findById(id)
+
+    if(!product){
+      res.status(404).json({msg: "Product not found"})
+      return;
+    }
+
+    res.json(product)
+
+  } catch (error) {
+  console.log(error)
+  }
+},
+
+delete: async (req,res) => {
+  try {
+    const id = req.params.id
+    const product = await ProductModel.findById(id)
+    if(!product){
+      res.status(404).json({msg: "Product not found"})
+    }
+
+    const deletedProduct = await ProductModel.findByIdAndDelete(id)
+    res.status(200).json(deletedProduct,{msg: "Product deleted successfully"})
+
+  } catch (error) {
+    console.log(error)
+  }
+
+},
+
+update: async (req, res) => {
+
+  const id = req.params.id
+  const product = {
+    title: req.body.title,
+    code: req.body.code,
+    size: req.body.size,
+    price: req.body.price
+  }
+
+  const updatedProduct = await ProductModel.findByIdAndUpdate(id,product)
+  if(!updatedProduct){
+    res.status(404).json({msg: "Product not found"})
+  }
+  res.status(200).json({product, msg:"Product Updated successfully!"})
 }
 
 
 
 
 
-
 }
+
+
+module.exports = productController;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,15 +2,15 @@ const router = require('express').Router()
 
 const productController = require('../controllers/productController')
 
-router.route('/product').post(()=> productController.create(req,res))
+router.route('/product').post((req,res)=> productController.create(req,res))
 
-router.route('/product').get(()=> productController.getAll(req,res))
+// router.route('/product').get(()=> productController.getAll(req,res))
 
-router.route('/product').get(()=> productController.get(req,res))
+// router.route('/product').get(()=> productController.get(req,res))
 
-router.route('/product').delete(()=> productController.delete(req,res))
+// router.route('/product').delete(()=> productController.delete(req,res))
 
-router.route('/product').put(()=> productController.update(req,res))
+// router.route('/product').put(()=> productController.update(req,res))
 
 
 module.exports = router;
